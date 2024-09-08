568fa715056fde798036ac72ecccdd5c6dd75de0
asmit9 <92090162+asmit9@users.noreply.github.com>
2024-09-08 15:31:48 +0530

    config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ load_dotenv()
 
 class Config(object):
     # required config variables
-    API_HASH = getenv("API_HASH", None)                # get from my.telegram.org
-    API_ID = int(getenv("API_ID", 0))                  # get from my.telegram.org
-    BOT_TOKEN = getenv("BOT_TOKEN", None)              # get from @BotFather
-    DATABASE_URL = getenv("DATABASE_URL", None)        # from https://cloud.mongodb.com/
-    HELLBOT_SESSION = getenv("HELLBOT_SESSION", None)  # enter your session string here
+    API_HASH = getenv("API_HASH", "0e017f716c49a52a0ba4a8bfa95ccaf7")                # get from my.telegram.org
+    API_ID = int(getenv("API_ID", "27896987"))                  # get from my.telegram.org
+    BOT_TOKEN = getenv("BOT_TOKEN", "7223926558:AAH-0Z2AIcyI6W3g8F-SuV06gBMzc07G_xM")              # get from @BotFather
+    DATABASE_URL = getenv("DATABASE_URL", "mongodb+srv://asmit9831:plmokn123@cluster0.1xdv6mh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")        # from https://cloud.mongodb.com/
+    HELLBOT_SESSION = getenv("HELLBOT_SESSION", "BQBa9TG4pi1R_fPMS7gqzjSsny6EYv70dsx_NiXsdfg8Ou1rpGw7mbt_srQtpw3z7PvWX2YcPnH0QXRMVZesgzy53sKP3Mf-3aeYRi1VazYnUzaO0J0v5fN6kBzi1u_skICRaue2Vj0uwllV5-_NwiRx13GbaKqk8ZbA0NjPwtou-GnMJDj324ZIwqnQrNckbGHvG_H9SdPsJ6D8FdKcoL3Ih2qNffYF8snCAnLdlAcd8iUF0fwc-8oHmAADuCg3TFDSAGwnFGQuQP90q8L0d8kIhEOtdeoNkqVdCgOv62zivKGYs5XL7L0sOSiDUNwiBSRkPnAeo-E6n7ZH8eGL8KOaAAAAAaVCZwUA")  # enter your session string here
     LOGGER_ID = int(getenv("LOGGER_ID", 0))            # make a channel and get its ID
-    OWNER_ID = getenv("OWNER_ID", "")                  # enter your id here
+    OWNER_ID = getenv("OWNER_ID", "7067559685")                  # enter your id here
 
     # optional config variables
     BLACK_IMG = getenv("BLACK_IMG", "https://telegra.ph/file/2c546060b20dfd7c1ff2d.jpg")        # black image for progress
